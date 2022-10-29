33669390386c9ac23012b788fbdc5a375e13137d
Youssef Moataz <yousefcrunshy03@gmail.com>
2022-10-29 18:46:26 +0200

    Added test functions to functions file.

--- FunctionsApp.cpp
+++ FunctionsApp.cpp
@@ -1,3 +1,19 @@
+/*
+FCAI - Programming 2 - 2022 - Assignment 2 - Sheet 2 - Problem 1
+
+Program Name:           Function Solver
+Program Description:    This program makes it possible to define a math function
+                        and return the output of three types of functions:
+                        -Simple Function -Composite Function -Derivative Function
+Last Modification Date: October 29, 2022
+Version:                v1.0.0
+Author:                 Youssef Moataz
+ID:                     20210488
+Group:                  A
+Labs:                   S10
+Purpose:                Make math calculations easier
+*/
+
 #include "FunctionsFunctions.cpp"
 
 int main() {
